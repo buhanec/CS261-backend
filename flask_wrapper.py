53c8267cb8aff5aeed323912e352feefd21cf287
buhanec <alen.buhanec@gmail.com>
2015-03-06 14:17:58 +0000

    tables done

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -11,23 +11,35 @@ from flask.ext.api import FlaskAPI, status, exceptions
 app = FlaskAPI(__name__)
 system = TheSystem()
 
+
 def api_repr(value):
     return {'response': value}
 
+
 @app.route("/plugins/", methods=['GET'])
-def f1(): return api_repr(system.plugins)
+def f1():
+    return api_repr(system.plugins)
+
 
 @app.route("/plugins/loaded/", methods=['GET'])
-def f2(): return api_repr(system.loaded_plugins)
+def f2():
+    return api_repr(system.loaded_plugins)
+
 
 @app.route("/plugins/loaded/input/", methods=['GET'])
-def f3(): return api_repr(system.loaded_input_plugins)
+def f3():
+    return api_repr(system.loaded_input_plugins)
+
 
 @app.route("/plugins/loaded/storage/", methods=['GET'])
-def f4(): return api_repr(system.loaded_storage_plugins)
+def f4():
+    return api_repr(system.loaded_storage_plugins)
+
 
 @app.route("/plugins/loaded/query/", methods=['GET'])
-def f5(): return api_repr(system.loaded_query_plugins)
+def f5():
+    return api_repr(system.loaded_query_plugins)
+
 
 @app.route("/plugins/loaded/<string:plugin>", methods=['PUT'])
 def f6(plugin):
@@ -35,8 +47,9 @@ def f6(plugin):
     desc = request.data.get('desc', None)
     args = request.data.get('args', ())
     plugin_id = system.load_plugin(name, desc, plugin, args)
-    #plugin_id = system.load_plugin(name, desc, plugin, 'sqlite://')
+    # plugin_id = system.load_plugin(name, desc, plugin, 'sqlite://')
     return api_repr(plugin_id)
+
 
 @app.route("/plugins/loaded/<int:id_>", methods=['DELETE'])
 def f7(id_):
@@ -46,8 +59,11 @@ def f7(id_):
     except:
         return api_repr(False)
 
+
 @app.route("/connections", methods=['GET'])
-def f8(): return api_repr(system.connections)
+def f8():
+    return api_repr(system.connections)
+
 
 @app.route("/connections/<int:input_>/<int:storage>", methods=['PUT'])
 def f9(input_, storage):
@@ -55,6 +71,7 @@ def f9(input_, storage):
         return api_repr(system.connect_plugins(input_, storage))
     except:
         return api_repr(False)
+
 
 @app.route("/connections/<int:tid>", methods=['DELETE'])
 def f10(tid):
@@ -64,8 +81,11 @@ def f10(tid):
     except:
         return api_repr(False)
 
+
 @app.route("/interface/", methods=['GET'])
-def f11(): return api_repr(system.interface_id)
+def f11():
+    return api_repr(system.interface_id)
+
 
 @app.route("/interface/<int:id_>", methods=['PUT'])
 def f12(id_):
@@ -75,12 +95,14 @@ def f12(id_):
     except:
         return api_repr(False)
 
+
 @app.route("/data/trades/<int:number>", methods=['GET'])
 def f13(number):
     try:
         return api_repr(system.interface.trades(None, number))
     except:
         return api_repr(False)
+
 
 @app.route("/data/trades/<string:query>/<int:number>", methods=['GET'])
 def f14(query, number):
@@ -89,12 +111,14 @@ def f14(query, number):
     except:
         return api_repr(False)
 
+
 @app.route("/data/comms/<int:number>", methods=['GET'])
 def f15(number):
     try:
         return api_repr(system.interface.comms(None, number))
     except:
         return api_repr(False)
+
 
 @app.route("/data/comms/<string:query>/<int:number>", methods=['GET'])
 def f16(query, number):
@@ -103,12 +127,14 @@ def f16(query, number):
     except:
         return api_repr(False)
 
+
 @app.route("/data/alerts/<int:number>", methods=['GET'])
 def f17(number):
     try:
         return api_repr(system.interface.alerts(None, number))
     except:
         return api_repr(False)
+
 
 @app.route("/data/alerts/<string:query>/<int:number>", methods=['GET'])
 def f18(query, number):
@@ -117,12 +143,14 @@ def f18(query, number):
     except:
         return api_repr(False)
 
+
 @app.route("/data/stock/<string:symbol>", methods=['GET'])
 def f19(symbol):
     try:
         return api_repr(system.interface.stock(symbol))
     except:
         return api_repr(False)
+
 
 @app.route("/data/trader/<string:email>", methods=['GET'])
 def f20(email):
@@ -131,12 +159,14 @@ def f20(email):
     except:
         return api_repr(False)
 
+
 @app.route("/data/alert/<int:alertid>", methods=['GET'])
 def f21(alertid):
     try:
         return api_repr(system.interface.alert(alertid))
     except:
         return api_repr(False)
+
 
 @app.route("/data/plot/<string:column1>/<string:column2>")
 def f22(column1, column2):
@@ -147,6 +177,17 @@ def f22(column1, column2):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    # Params for data collection
+    trades = ('cs261.dcs.warwick.ac.uk', 80)
+    comms = ('cs261.dcs.warwick.ac.uk', 1720)
+    # Load plugins
+    trades_id = system.load_plugin('Trades', None, 'NetCap', trades)
+    comms_id = system.load_plugin('Comms', None, 'NetCap', comms)
+    printer_id = system.load_plugin('Printer', None, 'Printer', None)
+    # Connect plugins
+    system.connect_plugins(trades_id, printer_id)
+    system.connect_plugins(comms_id, printer_id)
 
     print >> sys.stderr, "\n*** STACKTRACE - START ***"
     code = []
@@ -161,4 +202,3 @@ if __name__ == '__main__':
     for line in code:
         print >> sys.stderr, line
     print >> sys.stderr, "\n*** STACKTRACE - END ***\n"
-
--- plugins/sql_storage.py
+++ plugins/sql_storage.py
@@ -25,44 +25,156 @@ class SqlStorage(StoragePlugin, QueryPlugin, Plugin):
         self.factory = sessionmaker(bind=self.engine)
         self.Session = scoped_session(self.factory)
         metadata = sa.MetaData(self.engine)
-        tables = {
-            'traders': sa.Table(
-                'traders', metadata,
-                sa.Column('trader', st.String(254), nullable=False),
-                sa.Column('activity', st.DateTime(timezone=True),
-                          default=datetime.datetime.utcnow, nullable=False)
+        c = sa.Column
+        t = sa.Table
+        # Types used in tables for easier tweaking
+        price = st.Numeric(8, 2)
+        email = st.String(254)
+        date = st.DateTime(timezone=True)
+        symbol = st.CHAR(7)
+        sector = st.String(255)
+        volume = st.BigInteger()
+        day = st.SmallInteger()
+        firm = st.String(30)
+        currency = st.Enum(('GBX'))
+        percent = st.Numeric(3, 2)
+        spread = st.Numeric(3, 2)
+        perf = st.Numeric(3, 2)
+        num_dev = st.Numberic(2, 2)
+        trade_id = st.String(12)
+        group_id = st.BigInteger()
+        # All the tables
+        self.tables = {
+            'live_trade': t(
+                'live_trade', metadata,
+                c('time', date, nullable=False),
+                c('buyer', email, nullable=False),
+                c('seller', email, nullable=False),
+                c('price', price, nullable=False),
+                c('size', volume, nullable=False),
+                c('currency', currency, nullable=False),
+                c('symbol', symbol, nullable=False),
+                c('sector', sector, nullable=False),
+                c('bid', price, nullable=False),
+                c('ask', price, nullable=False),
+                c('day', day, nullable=False),
+                c('trade_id', trade_id, nullable=False, primary_key=True),
+                c('buyer_firm', firm),
+                c('seller_firm', firm)
             ),
-            'trades': sa.Table(
-                'trades', metadata,
-                sa.Column('time', st.DateTime(timezone=True),
-                          default=datetime.datetime.utcnow, nullable=False),
-                sa.Column('buyer', st.String(254), nullable=False),
-                sa.Column('seller', st.String(254), nullable=False),
-                sa.Column('price', st.Numeric(8, 2), nullable=False),
-                sa.Column('size', st.BigInteger(), nullable=False),
-                sa.Column('currency', st.Enum(('GBX')), nullable=False),
-                sa.Column('symbol', st.CHAR(6), nullable=False),
-                sa.Column('sector', st.String(255), nullable=False),
-                sa.Column('bid', st.Numeric(8, 2), nullable=False),
-                sa.Column('ask', st.Numeric(8, 2), nullable=False)
+            'live_comms': t(
+                'live_comms', metadata,
+                c('time', date, nullable=False),
+                c('sender', email, nullable=False),
+                c('group', group_id, nullable=False),
+                c('day', day, nullable=False)
             ),
-            'recipients': sa.Table(
+            'past_trade': t(
+                'past_trade', metadata,
+                c('time', date, nullable=False),
+                c('buyer', email, nullable=False),
+                c('seller', email, nullable=False),
+                c('price', price, nullable=False),
+                c('size', volume, nullable=False),
+                c('currency', currency, nullable=False),
+                c('symbol', symbol, nullable=False),
+                c('sector', sector, nullable=False),
+                c('bid', price, nullable=False),
+                c('ask', price, nullable=False),
+                c('day', day, nullable=False),
+                c('trade_id', trade_id, nullable=False, primary_key=True),
+                c('buyer_firm', firm),
+                c('seller_firm', firm)
+            ),
+            'past_comms': t(
+                'past_comms', metadata,
+                c('time', date, nullable=False),
+                c('sender', email, nullable=False),
+                c('group', group_id, nullable=False),
+                c('day', day, nullable=False)
+            ),
+            'recipients': t(
                 'recipients', metadata,
-                sa.Column('group', st.BigInteger(), nullable=False),
-                sa.Column('recipient', st.String(254), nullable=False)
+                c('group', group_id, nullable=False),
+                c('recipient', email, nullable=False)
             ),
-            'comms': sa.Table(
-                'comms', metadata,
-                sa.Column('time', st.DateTime(timezone=True),
-                          default=datetime.datetime.utcnow, nullable=False),
-                sa.Column('sender', st.String(254), nullable=False),
-                sa.Column('group', st.BigInteger(), nullable=False)
+            'averages_trade': t(
+                'averages_trade', metadata,
+                c('symbol', symbol, nullable=False, primary_key=True),
+                c('sector', sector, nullable=False),
+                c('one_day_close', price, nullable=False),
+                c('five_day_close', price, nullable=False),
+                c('twenty_day_close', price, nullable=False),
+                c('one_day_price_mean', price, nullable=False),
+                c('one_day_price_sd', price, nullable=False),
+                c('five_day_price_mean', price, nullable=False),
+                c('five_day_price_sd', price, nullable=False),
+                c('twenty_day_price_mean', price, nullable=False),
+                c('twenty_day_price_sd', price, nullable=False),
+                c('one_day_volume_mean', price, nullable=False),
+                c('one_day_volume_sd', price, nullable=False),
+                c('five_day_volume_mean', price, nullable=False),
+                c('five_day_volume_sd', price, nullable=False),
+                c('twenty_day_volume_mean', price, nullable=False),
+                c('twenty_day_volume_sd', price, nullable=False),
+                c('one_day_spread_mean', spread, nullable=False),
+                c('one_day_spread_sd', spread, nullable=False),
+                c('five_day_spread_mean', spread, nullable=False),
+                c('five_day_spread_sd', spread, nullable=False),
+                c('twenty_day_spread_mean', spread, nullable=False),
+                c('twenty_day_spread_sd', spread, nullable=False),
+                c('one_day_stock_perf', perf, nullable=False),
+                c('one_day_sector_perf', perf, nullable=False),
+                c('five_day_stock_perf', perf, nullable=False),
+                c('five_day_sector_perf', perf, nullable=False),
+                c('twenty_day_stock_perf', perf, nullable=False),
+                c('twenty_day_sector_perf', perf, nullable=False),
+                c('one_day_price_input', num_dev, nullable=False),
+                c('five_day_price_input', num_dev, nullable=False),
+                c('twenty_day_price_input', num_dev, nullable=False),
+                c('one_day_spread_input', num_dev, nullable=False),
+                c('five_day_spread_input', num_dev, nullable=False),
+                c('twenty_day_spread_input', num_dev, nullable=False),
+                c('one_day_volume_input', num_dev, nullable=False),
+                c('five_day_volume_input', num_dev, nullable=False),
+                c('twenty_day_volume_input', num_dev, nullable=False),
+                c('one_day_perf_input', percent, nullable=False),
+                c('five_day_perf_input', percent, nullable=False),
+                c('twenty_day_perf_input', percent, nullable=False),
+                c('holdings_input', percent, nullable=False),
+            ),
+            'portfolio_holdings': t(
+                'portfolio_holdings', metadata,
+                c('firm', firm, nullable=False),
+                c('symbol', symbol, nullable=False),
+                c('holdings', volume, nullable=False),
+                c('holdings_perc', percent, nullable=False)
+            ),
+            'suspicious_trade': t(
+                'suspicious_trade', metadata,
+                c('off_market_trans_trade_id', trade_id, nullable=False),
+                c('wash_trade_trade_id', trade_id, nullable=False),
+                c('one_day_price_trade_id', trade_id, nullable=False),
+                c('five_day_price_trade_id', trade_id, nullable=False),
+                c('twenty_day_price_trade_id', trade_id, nullable=False),
+                c('one_day_spread_trade_id', trade_id, nullable=False),
+                c('five_day_spread_trade_id', trade_id, nullable=False),
+                c('twenty_day_spread_trade_id', trade_id, nullable=False),
+                c('one_day_volume_trade_id', trade_id, nullable=False),
+                c('five_day_volume_trade_id', trade_id, nullable=False),
+                c('twenty_day_volume_trade_id', trade_id, nullable=False),
+                c('one_day_perf_trade_id', trade_id, nullable=False),
+                c('five_day_perf_trade_id', trade_id, nullable=False),
+                c('twenty_day_perf_trade_id', trade_id, nullable=False),
+                c('portfolio_firm', firm, nullable=False),
+                c('portfolio_symbol', symbol, nullable=False),
+                c('trader_activity_trade_id', trade_id, nullable=False),
             )
         }
 
-        for table in tables:
+        for table in self.tables:
             if not self.engine.dialect.has_table(self.engine.connect(), table):
-                tables[table].create(checkfirst=True)
+                self.tables[table].create(checkfirst=True)
 
         self.status = Plugin.STATUS_INIT
         self.logger.info('[SqlStorage] init')
@@ -71,8 +183,8 @@ class SqlStorage(StoragePlugin, QueryPlugin, Plugin):
         while not hasattr(self, "Session"):
             time.sleep(0.1)
         session = self.Session()
-        while not self._terminate.isSet():
         # while true # - should we let it clean up?
+        while not self._terminate.isSet():
             data = self._q.get()
             if data is None:  # flush blocked threads
                 self._q.task_done()
@@ -81,11 +193,24 @@ class SqlStorage(StoragePlugin, QueryPlugin, Plugin):
             self._q.task_done()
         self.Session.remove()
 
-    def burst_store(self, data, storage):
+    def store_trades(self, data):
+        """ Stores trades """
+
+    def store_comms(self, data):
+        """ Stores comms """
+        time = datetime.strptime(data[0], '%Y-%m-%d %H:%M:%S.%f')
+        sender = data[1]
+        sender_firm = data[1].rsplit('@', 1)[0]
+        recipients = [(r, r.rsplit('@', 1)[0]) for r in data[2].split(';')]
+        # SELECT MAX(Id) FROM Customers
+        #
+
+    def store(self, data):
         """ Performs queries to insert data """
-        # session.query(...)
-        # session.add(...)
-        # session.commit()
+        if len(data) == 10:
+            self.store_tradse(data)
+        elif len(data) == 3:
+            self.store_comms(data)
 
     def unload(self):
         super(SqlStorage, self).unload()
